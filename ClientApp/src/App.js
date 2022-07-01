2516ee015fba46064cabe30adc20506d949e4ca7
Hnatiuk Serhiy <hnatiuk.fw@gmail.com>
2022-07-01 13:07:47 +0300

    Created a project

---
+++ ClientApp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div>
+      Hello World!
+      <p>Our team:</p>
+      <ul>
+        <li>Volodymyr</li>
+        <li>Serhiy</li>
+        <li>Vladislav</li>
+        <li>Oleksandr</li>
+      </ul>
+    </div>
+  );
+}
